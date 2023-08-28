c6da4161d21798ef804ded9f10304b44c117c5f7
Mohammad.R Zolghadri <52266113+zolghadri@users.noreply.github.com>
2023-08-28 14:05:45 +0330

    fix: JsendModel Validation

--- jsend/response.py
+++ jsend/response.py
@@ -1,19 +1,18 @@
 from pydantic import BaseModel
 from typing import Optional, Any
 
-
 class JsendBase:
     status: str
     data: Any
 
     # Error
-    code: Optional[int]
-    message: Optional[str]
+    code: Optional[int] = None
+    message: Optional[str] = None
 
     #  Pagination
-    total: Optional[int]
-    count: Optional[int]
-    offset: Optional[int]
+    total: Optional[int] = None
+    count: Optional[int] = None
+    offset: Optional[int] = None
 
 
 class JsendResponse(JsendBase, BaseModel):
